246ca959b71ff4315452d2096a5611c8ca4070fa
Sulayman Shafi <sully@iPhone-14-pro-2.router>
2023-10-19 09:57:30 -0500

    inital comit

---
+++ src/loops/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        for (int i=0; i<5;i++)
+        {
+            System.out.println("Hello in for");
+        }
+
+        int x=0;
+        do
+        {
+            System.out.print("in while");
+            x++;
+        }
+        while (x<3);
+        }
+    }
